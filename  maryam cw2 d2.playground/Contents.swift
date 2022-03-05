656a0090c9b707c88735c2a3e6435681e4071521
maryam matiri <64448234+Lalique-art@users.noreply.github.com>
2022-03-05 13:46:16 +0300

    done

---
+++  maryam cw2 d2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let Name = "maryam"
+var Hobby = "reading"
+var FavNumber = 7
+var hight = 1.50
+print("hello,i am \(Name),my fav number is \(FavNumber),I love \(Hobby) ")
+
+
+
